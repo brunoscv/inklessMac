872a02f62baa7f11791f50a98f406e8908b790bc
Bruno Carvalho <brunocarvalho038@gmail.com>
2022-05-24 22:23:51 -0300

    colocando permissao de camera no android

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -93,27 +93,27 @@ export default function Profile({ route, navigation }) {
   }
   /** FIREBASE NOTIFICATION NAVIGATOR */
 
-  const requestCameraPermission = async () => {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: "App Camera Permission",
-          message:"App needs access to your camera ",
-          buttonNeutral: "Ask Me Later",
-          buttonNegative: "Cancel",
-          buttonPositive: "OK"
+    const requestCameraPermission = async () => {
+        try {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA,
+            {
+            title: "App Camera Permission",
+            message:"App needs access to your camera ",
+            buttonNeutral: "Ask Me Later",
+            buttonNegative: "Cancel",
+            buttonPositive: "OK"
+            }
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log("Camera permission given");
+        } else {
+            console.log("Camera permission denied");
         }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("Camera permission given");
-      } else {
-        console.log("Camera permission denied");
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  };
+        } catch (err) {
+        console.warn(err);
+        }
+    };
 
     const [resourcePath, setResourcePath] = useState();
     const [fileName, setFileName] = useState();
